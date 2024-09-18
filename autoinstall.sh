67d722862b6870675cb1c5201b5dcc9f9a7de6d7
Ardgellan <aegiev01@gmail.com>
2024-09-18 15:56:15 +0300

    autoinstall_commit

--- autoinstall.sh
+++ autoinstall.sh
@@ -182,7 +182,7 @@ echo "............................................................"
 echo "$Defaul_color" | sed 's/\$//g'
 
 #install xray
-bash -c "$(curl -L https://raw.githubusercontent.com/Ardgellan/XTLS_Reality_Server/main/install-release.sh)" @ install
+bash -c "$(curl -L https://raw.githubusercontent.com/Ardgellan/XTLS_Reality_Server/develop/install-release.sh)" @ install
 
 # To increase performance, you can configure
 # Bottleneck Bandwidth and
