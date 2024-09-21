e820490069365da4fa74f58d1a4c290a07999966
Ardgellan <aegiev01@gmail.com>
2024-09-21 18:53:13 +0300

    maraphet_18

--- source/handlers/user/infostand.py
+++ source/handlers/user/infostand.py
@@ -9,27 +9,40 @@ from source.middlewares import rate_limit
 from .check_is_user_banned import is_user_banned
 
 
+# @rate_limit(limit=1)
+# @is_user_banned
+# async def show_about_us(call: types.CallbackQuery):
+#     await call.message.edit_text(
+#         text=localizer.get_user_localized_text(
+#             user_language_code=call.from_user.language_code,
+#             text_localization=localizer.message.about_us_message,
+#         ),
+#         reply_markup=await inline.insert_button_back_to_main_menu(language_code=call.from_user.language_code)
+#     )
+#     await call.answer()
+
+
+# @rate_limit(limit=1)
+# @is_user_banned
+# async def show_terms_of_use(call: types.CallbackQuery):
+#     await call.message.edit_text(
+#         text=localizer.get_user_localized_text(
+#             user_language_code=call.from_user.language_code,
+#             text_localization=localizer.message.terms_of_use_message,
+#         ),
+#         reply_markup=await inline.insert_button_back_to_main_menu(language_code=call.from_user.language_code)
+#     )
+#     await call.answer()
+
 @rate_limit(limit=1)
 @is_user_banned
 async def show_about_us(call: types.CallbackQuery):
-    await call.message.edit_text(
-        text=localizer.get_user_localized_text(
-            user_language_code=call.from_user.language_code,
-            text_localization=localizer.message.about_us_message,
-        ),
-        reply_markup=await inline.insert_button_back_to_main_menu(language_code=call.from_user.language_code)
-    )
+    await call.message.edit_text("SALAM")
     await call.answer()
 
 
 @rate_limit(limit=1)
 @is_user_banned
 async def show_terms_of_use(call: types.CallbackQuery):
-    await call.message.edit_text(
-        text=localizer.get_user_localized_text(
-            user_language_code=call.from_user.language_code,
-            text_localization=localizer.message.terms_of_use,
-        ),
-        reply_markup=await inline.insert_button_back_to_main_menu(language_code=call.from_user.language_code)
-    )
+    await call.message.edit_text("SALAM")
     await call.answer()
