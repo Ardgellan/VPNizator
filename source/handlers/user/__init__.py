553bd9093bd19c671bad775ff1495c02d1d585f7
Ardgellan <aegiev01@gmail.com>
2024-09-27 13:10:07 +0300

    polishing_7

--- source/handlers/user/__init__.py
+++ source/handlers/user/__init__.py
@@ -2,16 +2,16 @@ from aiogram import Dispatcher
 from aiogram.types import ContentType
 from loguru import logger
 
-from .ask_support import *
-from .banners import about_us_function, terms_of_use_function
-from .configs_menu import register_configs_menu_handlers
-from .configs_menu.show_configs import *
-from .guide import register_show_help_guide_handlers
-from .my_profile import show_my_profile
 from .start import start, main_menu_by_button
+from .pay import show_balance_top_up_menu_function, handle_payment, process_pre_checkout_query, successful_payment
+from .configs_menu.show_configs import show_user_configs
 from .trial import trial_period_function, start_trial_period_function
 from .subscription import manual_renew_subscription
-from .pay import show_balance_top_up_menu_function, handle_payment, process_pre_checkout_query, successful_payment
+from .banners import about_us_function, terms_of_use_function
+from .ask_support import *
+
+from .configs_menu import register_configs_menu_handlers
+from .guide import register_show_help_guide_handlers
 
 def register_user_handlers(dp: Dispatcher):
     try:
@@ -55,47 +55,40 @@ def register_user_handlers(dp: Dispatcher):
 
         # 8
         dp.register_callback_query_handler(
-            show_my_profile,
-            lambda call: call.data == "my_profile",
-            state="*",
-        )
-
-        # 9
-        dp.register_callback_query_handler(
             ask_user_for_question_to_support,
             lambda call: call.data == "create_support_ticket",
             state="*",
         )
 
-        # 10
+        # 9
         dp.register_callback_query_handler(
             trial_period_function,
             lambda call: call.data == "trial_period",
             state="*",
         )
 
-        # 11
+        # 10
         dp.register_callback_query_handler(
             start_trial_period_function,
             lambda call: call.data == "start_trial_period",
             state="*",
         )
 
-        # 12
+        # 11
         dp.register_callback_query_handler(
             terms_of_use_function,
             lambda call: call.data == "terms_of_use",
             state="*",
         )
 
-        # 13
+        # 12
         dp.register_callback_query_handler(
             about_us_function,
             lambda call: call.data == "about_us",
             state="*",
         )
 
-        # 14
+        # 13
         dp.register_message_handler(
             forward_question_to_admins,
             state=AskSupport.waiting_for_question,
