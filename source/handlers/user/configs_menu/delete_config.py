1d4b522a46b241ea90754e3b4af90a8c1c18ad06
Ardgellan <aegiev01@gmail.com>
2024-09-30 00:38:11 +0300

    polishing_58

--- source/handlers/user/configs_menu/delete_config.py
+++ source/handlers/user/configs_menu/delete_config.py
@@ -23,23 +23,27 @@ async def confirm_delete_config(call: types.CallbackQuery, state: FSMContext):
 
 
 async def delete_config(call: types.CallbackQuery, state: FSMContext):
-    
     config_uuid = call.data.split("_")[-1]
-
     await xray_config.disconnect_user_by_uuid(uuid=config_uuid)
-    
-
     await call.message.edit_text(
         text=localizer.get_user_localized_text(
             user_language_code=call.from_user.language_code,
             text_localization=localizer.message.config_deleted,
         ),
         parse_mode=types.ParseMode.HTML,
-        reply_markup=await inline.back_to_main_menu_keyboard(
-            language_code=call.from_user.language_code
-        ), 
     )
     await call.answer()
     await call.message.delete()
-    
+    await call.message.answer(
+        text=localizer.get_user_localized_text(
+            user_language_code=call.from_user.language_code,
+            text_localization=localizer.message.user_configs_list,
+        ),
+        parse_mode=types.ParseMode.HTML,
+        reply_markup=await inline.user_configs_list_keyboard(
+            user_id=call.from_user.id,
+            language_code=call.from_user.language_code,
+            show_create_new_config_button=True,
+        ),
+    )
     await state.finish()
