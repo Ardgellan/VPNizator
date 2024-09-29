4d5f883f684cb19c411f5f8a957e4ba5e3ff472d
Ardgellan <aegiev01@gmail.com>
2024-09-30 00:57:36 +0300

    polishing_62

--- source/handlers/user/configs_menu/delete_config.py
+++ source/handlers/user/configs_menu/delete_config.py
@@ -33,17 +33,17 @@ async def delete_config(call: types.CallbackQuery, state: FSMContext):
         parse_mode=types.ParseMode.HTML,
     )
     await call.answer()
-    await call.message.delete()
-    await call.message.answer(
-        text=localizer.get_user_localized_text(
-            user_language_code=call.from_user.language_code,
-            text_localization=localizer.message.user_configs_list,
-        ),
-        parse_mode=types.ParseMode.HTML,
-        reply_markup=await inline.user_configs_list_keyboard(
-            user_id=call.from_user.id,
-            language_code=call.from_user.language_code,
-            show_create_new_config_button=True,
-        ),
-    )
+    # await call.message.delete()
+    # await call.message.answer(
+    #     text=localizer.get_user_localized_text(
+    #         user_language_code=call.from_user.language_code,
+    #         text_localization=localizer.message.user_configs_list,
+    #     ),
+    #     parse_mode=types.ParseMode.HTML,
+    #     reply_markup=await inline.user_configs_list_keyboard(
+    #         user_id=call.from_user.id,
+    #         language_code=call.from_user.language_code,
+    #         show_create_new_config_button=True,
+    #     ),
+    # )
     await state.finish()
