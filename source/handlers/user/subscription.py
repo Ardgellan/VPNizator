8242800cfc6626358a38715eb7ed63ca104b3e86
Ardgellan <aegiev01@gmail.com>
2024-10-09 00:39:22 +0300

    mat_peremat_8

--- source/handlers/user/subscription.py
+++ source/handlers/user/subscription.py
@@ -11,11 +11,10 @@ from source.keyboard import inline
 
 async def manual_renew_subscription(call: types.CallbackQuery, state: FSMContext):
 
-    # Узнаем, есть ли у юзера вообще конфиги, чтобы их обновлять
+    # Узнаем есть ли у юзера вообще конфиги чтобы их обновлять
     user_id = call.from_user.id
-    logger.debug("Step_1")
     configs_to_renew = await db_manager.is_user_have_any_config(user_id)
-    logger.debug("Step_2")
+
     if not configs_to_renew:
         await call.message.answer(
             text=localizer.get_user_localized_text(
@@ -26,11 +25,10 @@ async def manual_renew_subscription(call: types.CallbackQuery, state: FSMContext
         )
         await call.answer()
         return
-    logger.debug("Step_3")
 
     # Проверяем время последнего платежа
     subscription_is_active = await db_manager.get_subscription_status(user_id)
-    logger.debug("Step_4")
+
     if subscription_is_active:
         await call.message.answer(
             text=localizer.get_user_localized_text(
@@ -41,46 +39,24 @@ async def manual_renew_subscription(call: types.CallbackQuery, state: FSMContext
         )
         await call.answer()
         return
-    logger.debug("Step_5")
 
     # Получаем текущий баланс и стоимость подписки
     current_balance = await db_manager.get_user_balance(user_id)
-    logger.debug("Step_6")
     subscription_cost = await db_manager.get_current_subscription_cost(user_id)
-    logger.debug("Step_7")
-    if current_balance >= subscription_cost:  # Исправленный отступ
-        async with db_manager.transaction() as conn:
-            try:
-                # Продлеваем подписку: обновляем баланс и время последнего платежа
-                logger.debug("Step_8")
-                await db_manager.update_user_balance(user_id, -subscription_cost, conn=conn)
-                logger.debug("Step_9")
-                await db_manager.update_last_subscription_payment(
-                    user_id, datetime.now(), conn=conn
-                )
-                logger.debug("Step_10")
 
-                # Если оба обновления прошли успешно, восстанавливаем конфиги пользователя в Xray
-                await xray_config.reactivate_user_configs_in_xray([user_id])
-                logger.debug("Step_11")
-
-                await call.message.answer(
-                    text=localizer.get_user_localized_text(
-                        user_language_code=call.from_user.language_code,
-                        text_localization=localizer.message.subscription_renewed_successfully,
-                    ),
-                    parse_mode=types.ParseMode.HTML,
-                )
-            except Exception as e:
-                # Если произошла ошибка, транзакция откатывается, и конфиги не восстанавливаются
-                logger.error(f"Error during subscription renewal for user {user_id}: {str(e)}")
-                await call.message.answer(
-                    text=localizer.get_user_localized_text(
-                        user_language_code=call.from_user.language_code,
-                        text_localization=localizer.message.error_occurred_during_sub_renewal,
-                    ),
-                    parse_mode=types.ParseMode.HTML,
-                )
+    if current_balance >= subscription_cost:
+        # Продлеваем подписку
+        await db_manager.update_user_balance(user_id, -subscription_cost)
+        await db_manager.update_last_subscription_payment(user_id, datetime.now())
+        # Восстанавливаем конфиги пользователя в Xray
+        await xray_config.reactivate_user_configs_in_xray([user_id])
+        await call.message.answer(
+            text=localizer.get_user_localized_text(
+                user_language_code=call.from_user.language_code,
+                text_localization=localizer.message.subscription_renewed_successfully,
+            ),
+            parse_mode=types.ParseMode.HTML,
+        )
     else:
         # Недостаточно средств
         await call.message.answer(
