46783355362bea841bec93051b7e24bafed5b40e
Ardgellan <aegiev01@gmail.com>
2024-10-09 01:13:46 +0300

    mat_peremat_9

--- source/database/updater.py
+++ source/database/updater.py
@@ -10,26 +10,6 @@ class Updater(DatabaseConnector):
         super().__init__()
         logger.debug("Updater object was initialized")
 
-    # async def add_days_to_user_subscription(self, user_id: int, days: int) -> bool:
-    #     """
-    #     Add days to user subscription if subscription_end_date >= current_date
-    #     else set subscription_end_date to current_date + days
-    #     """
-    #     query = f"""--sql
-    #         UPDATE users
-    #         SET subscription_end_date = CASE
-    #             WHEN subscription_end_date >= CURRENT_DATE
-    #             THEN subscription_end_date + INTERVAL '{days} days'
-    #             ELSE CURRENT_DATE + INTERVAL '{days} days'
-    #         END
-    #         WHERE user_id = {user_id};
-    #     """
-    #     if await self._execute_query(query) is False:
-    #         logger.error(f"Error while adding {days} days to user {user_id} subscription")
-    #         return False
-    #     logger.debug(f"Added {days} days to user {user_id} subscription")
-    #     return True
-
     async def toggle_user_banned_status(self, user_id: int) -> bool:
         """
         Toggle user banned status
@@ -45,31 +25,6 @@ class Updater(DatabaseConnector):
         logger.debug(f"Toggled user {user_id} banned status")
         return True
 
-    # async def upsert_bonus_config_generations_to_user(
-    #     self, user_id: int, new_bonus_config_count: int
-    # ):
-    #     query = f"""--sql
-    #         INSERT INTO bonus_configs_for_users (user_id, bonus_config_count)
-    #         VALUES ({user_id}, {new_bonus_config_count})
-    #         ON CONFLICT (user_id)
-    #         DO UPDATE SET bonus_config_count = {new_bonus_config_count};
-    #     """
-    #     if await self._execute_query(query) is False:
-    #         logger.error(f"Error while setting bonus config generations to user {user_id}")
-    #         return False
-    #     logger.debug(f"Set bonus config generations to user {user_id}")
-    #     return True
-
-    # async def mark_trial_as_used(self, user_id: int):
-    #     query = f"""--sql
-    #         UPDATE users
-    #         SET trial_used = TRUE
-    #         WHERE user_id = {user_id};
-    #     """
-    #     await self._execute_query(query)
-    #     logger.debug(f"User {user_id} marked as having used the trial period")
-    #     return True
-
     async def mark_trial_as_used(self, user_id: int, conn=None):
         query = f"""--sql
             UPDATE users
@@ -84,19 +39,6 @@ class Updater(DatabaseConnector):
             await self._execute_query(query)
         logger.debug(f"User {user_id} marked as having used the trial period")
         return True
-
-    # async def update_user_balance(self, user_id: int, amount: float) -> bool:
-    #     """Обновляем баланс пользователя, добавляя или вычитая средства"""
-    #     query = f"""--sql
-    #         UPDATE users
-    #         SET balance = balance + {amount}
-    #         WHERE user_id = {user_id};
-    #     """
-    #     if await self._execute_query(query) is False:
-    #         logger.error(f"Error while updating balance for user {user_id}")
-    #         return False
-    #     logger.debug(f"Updated balance for user {user_id} by {amount}")
-    #     return True
 
     async def update_user_balance(self, user_id: int, amount: float, conn=None) -> bool:
         query = f"""--sql
@@ -128,26 +70,6 @@ class Updater(DatabaseConnector):
         logger.debug(f"User {user_id} last subscription payment updated to {payment_time}")
         return True
 
-    # async def update_last_subscription_payment(
-    #     self, user_id: int, payment_time: datetime, conn=None
-    # ) -> bool:
-    #     """Обновляем время последнего платежа для пользователя по его user_id"""
-    #     query = f"""--sql
-    #         UPDATE users
-    #         SET last_subscription_payment = '{payment_time}'
-    #         WHERE user_id = {user_id};
-    #     """
-    #     if conn:
-    #         # Если передано соединение, выполняем запрос через него
-    #         await conn.execute(query)
-    #     else:
-    #         # Если соединение не передано, создаем новое и выполняем запрос
-    #         if await self._execute_query(query) is False:
-    #             logger.error(f"Error while updating last subscription payment for user {user_id}")
-    #             return False
-    #     logger.debug(f"User {user_id} last subscription payment updated to {payment_time}")
-    #     return True
-
     async def update_subscription_status(self, user_id: int, is_active: bool):
         query = f"""--sql
             UPDATE users
@@ -156,34 +78,6 @@ class Updater(DatabaseConnector):
         """
         await self._execute_query(query)
         logger.info(f"Статус подписки для пользователя {user_id} обновлен на {is_active}.")
-
-    # async def update_subscription_status(self, user_id: int, is_active: bool):
-    #     query = f"""--sql
-    #         UPDATE users
-    #         SET subscription_is_active = {is_active}
-    #         WHERE user_id = {user_id};
-    #     """
-    #     try:
-    #         await self._execute_query(query)  # Выполняем запрос
-    #         logger.info(f"Статус подписки для пользователя {user_id} обновлен на {is_active}.")
-    #     except Exception as e:
-    #         # Логируем ошибку и пробрасываем исключение дальше
-    #         logger.error(
-    #             f"Ошибка при обновлении статуса подписки для пользователя {user_id}: {str(e)}"
-    #         )
-    #         raise  # Пробрасываем исключение дальше для обработки в вызывающей функции
-
-    async def update_subscription_status_for_users(self, users_ids: list[int], is_active: bool):
-        """
-        Массово обновляем статус подписки для списка пользователей.
-        """
-        query = """
-            UPDATE users
-            SET subscription_is_active = $2
-            WHERE user_id = ANY($1);
-        """
-        await self._execute_query(query, users_ids, is_active)
-        logger.info(f"Статус подписки для пользователей {users_ids} обновлен на {is_active}.")
 
     # async def update_subscription_status_for_users(self, users_ids: list[int], is_active: bool):
     #     """
@@ -194,21 +88,22 @@ class Updater(DatabaseConnector):
     #         SET subscription_is_active = $2
     #         WHERE user_id = ANY($1);
     #     """
-    #     try:
-    #         await self._execute_query(query, users_ids, is_active)  # Выполняем запрос
-    #         logger.info(f"Статус подписки для пользователей {users_ids} обновлен на {is_active}.")
-    #     except Exception as e:
-    #         # Логируем ошибку и пробрасываем исключение дальше
-    #         logger.error(
-    #             f"Ошибка при массовом обновлении статуса подписки для пользователей {users_ids}: {str(e)}"
-    #         )
-    #         raise  # Пробрасываем исключение для обработки в вызывающей функции
+    #     await self._execute_query(query, users_ids, is_active)
+    #     logger.info(f"Статус подписки для пользователей {users_ids} обновлен на {is_active}.")
 
-    async def save_user_payment_method(self, user_id: int, payment_method_id: str):
-        query = f"""--sql
-            UPDATE users
-            SET payment_method_id = '{payment_method_id}'
-            WHERE user_id = {user_id};
+    async def update_subscription_status_for_users(self, users_ids: list[int], is_active: bool):
         """
-        await self._execute_query(query)
-        logger.debug(f"Payment method ID for user {user_id} saved: {payment_method_id}")
+        Массово обновляем статус подписки для списка пользователей.
+        """
+        query = """
+            UPDATE users
+            SET subscription_is_active = $2
+            WHERE user_id = ANY($1);
+        """
+        try:
+            await self._execute_query(query, users_ids, is_active)  # Выполняем запрос
+            logger.info(f"Статус подписки для пользователей {users_ids} обновлен на {is_active}.")
+        except Exception as e:
+            # Логируем ошибку и пробрасываем исключение дальше
+            logger.error(f"Ошибка при массовом обновлении статуса подписки для пользователей {users_ids}: {str(e)}")
+            raise  # Пробрасываем исключение для обработки в вызывающей функции
--- source/handlers/user/pay.py
+++ source/handlers/user/pay.py
@@ -99,7 +99,6 @@ async def handle_payment(call: types.CallbackQuery):
 
 async def create_payment(amount, chat_id):
     id_key = str(uuid.uuid4())
-    logger.debug("ПРОВЕРКА_1")
     payment = Payment.create(
         {
             "amount": {"value": amount, "currency": "RUB"},
@@ -121,7 +120,6 @@ async def create_payment(amount, chat_id):
         },
         id_key,
     )
-    logger.debug("ПРОВЕРКА_2")
     return payment.confirmation.confirmation_url, payment.id
 
 
@@ -172,138 +170,3 @@ async def check_payment_status(payment_id, chat_id, amount):
     elif payment["status"] == "canceled":
         logger.info(f"Платеж {payment_id} был отменен для пользователя {chat_id}.")
         return False
-
-
-# async def handle_payment(call: types.CallbackQuery):
-#     # Получаем сумму из callback_data
-#     amount_mapping = {
-#         "pay_50_rubles": 50,
-#         "pay_100_rubles": 100,
-#         "pay_300_rubles": 300,
-#         "pay_500_rubles": 500,
-#         "pay_700_rubles": 700,
-#         "pay_1000_rubles": 1000,
-#         "pay_3000_rubles": 3000,
-#     }
-#     amount = amount_mapping.get(call.data)  # Получаем сумму по нажатой кнопке
-
-#     if amount is not None:
-#         # Проверяем, есть ли у пользователя сохраненный метод оплаты
-#         payment_method_id = await db_manager.get_user_payment_method(call.from_user.id)
-
-#         if payment_method_id:
-#             # Если есть сохраненный метод, создаем платеж с его использованием
-#             payment_url, payment_id = await create_payment(
-#                 amount, call.from_user.id, payment_method_id
-#             )
-#         else:
-#             # Если нет сохраненного метода, создаем обычный платеж
-#             payment_url, payment_id = await create_payment(amount, call.from_user.id)
-
-#         if payment_url:
-#             await call.message.answer(
-#                 text=localizer.get_user_localized_text(
-#                     user_language_code=call.from_user.language_code,
-#                     text_localization=localizer.message.payment_confirmation_message,
-#                 ),
-#                 parse_mode=types.ParseMode.HTML,
-#                 reply_markup=await inline.payment_confirmation_keyboard(
-#                     language_code=call.from_user.language_code, payment_url=payment_url
-#                 ),
-#             )
-
-#             # Запуск проверки статуса платежа
-#             payment_success = await check_payment_status(payment_id, call.from_user.id, amount)
-#             if payment_success:
-#                 await call.message.answer(
-#                     text=localizer.get_user_localized_text(
-#                         user_language_code=call.from_user.language_code,
-#                         text_localization=localizer.message.successfull_payment_message,
-#                     ).format(amount=amount),
-#                     reply_markup=await inline.insert_button_back_to_main_menu(
-#                         language_code=call.from_user.language_code
-#                     ),
-#                 )
-#             else:
-#                 await call.message.answer(f"Ваш платеж был отменен.")
-#         else:
-#             await call.message.answer(
-#                 text=localizer.get_user_localized_text(
-#                     user_language_code=call.from_user.language_code,
-#                     text_localization=localizer.message.payment_assembly_error_message,
-#                 ),
-#                 parse_mode=types.ParseMode.HTML,
-#                 reply_markup=await inline.insert_button_back_to_main_menu(
-#                     language_code=call.from_user.language_code
-#                 ),
-#             )
-#     else:
-#         await call.message.answer("Неизвестная сумма. Пожалуйста, попробуйте снова.")
-
-#     await call.answer()  # Подтверждаем обработку коллбека
-
-
-# async def create_payment(amount, chat_id, payment_method_id=None):
-#     id_key = str(uuid.uuid4())
-#     logger.debug("ПРОВЕРКА_1")
-#     payment_data = {
-#         "amount": {
-#             "value": amount,
-#             "currency": "RUB"
-#             },
-#         "payment_method_data": {
-#             "type": "bank_card"
-#             },
-#         "confirmation": {
-#             "type": "redirect",
-#             "return_url": "https://t.me/VPNizatorBot"
-#             },
-#         "capture": True,
-#         "metadata": {
-#             "chat_id": chat_id
-#             },
-#         "description": "Пополнение баланса VPNizator",
-#     }
-#     logger.debug("ПРОВЕРКА_2")
-#     # Если у пользователя нет сохраненного метода оплаты, добавляем параметр save_payment_method
-#     if payment_method_id is None:
-#         payment_data["save_payment_method"] = True
-#         logger.debug(f"Creating payment with save_payment_method=True for user {chat_id}")
-#     else:
-#         payment_data["payment_method_id"] = payment_method_id
-#         logger.debug(f"Creating payment with payment_method_id={payment_method_id} for user {chat_id}")
-#     logger.debug(f"ULTRABABASRAKA! Full payment data before sending: {payment_data}")
-#     payment = Payment.create(payment_data, id_key)
-#     logger.debug(f"ULTRABABASRAKA! Payment created: {payment.json()}")
-#     logger.debug(f"Payment created: {payment.json()}")
-#     return payment.confirmation.confirmation_url, payment.id
-
-
-# async def check_payment_status(payment_id, chat_id, amount):
-#     # max_attempts = 120  # Максимальное количество попыток (например, 10 минут с шагом 5 секунд)
-#     # attempts = 0
-#     # Опрос API ЮKassa на предмет статуса платежа
-#     payment = json.loads((Payment.find_one(payment_id)).json())
-#     # Логируем ответ на первый запрос к ЮKassa
-#     logger.debug(f"Initial payment status for payment_id={payment_id}: {payment}")
-
-#     while payment["status"] == "pending":  # and attempts < max_attempts
-#         logger.info(f"Платеж {payment_id} для пользователя {chat_id} находится в ожидании.")
-#         await asyncio.sleep(5)  # Ожидание 5 секунд перед следующим запросом
-#         payment = json.loads((Payment.find_one(payment_id)).json())
-#         # attempts += 1
-#         logger.debug(f"Updated payment status for payment_id={payment_id}: {payment}")
-
-#     if payment["status"] == "succeeded":
-#         logger.info(f"Платеж {payment_id} успешно выполнен пользователем {chat_id}.")
-#         # Сохраняем payment_method_id, если он есть
-#         if "payment_method" in payment:
-#             logger.debug(f"Saving payment_method_id={payment['payment_method']['id']} for user {chat_id}")
-#             await db_manager.save_user_payment_method(chat_id, payment["payment_method"]["id"])
-
-#         # Обновляем баланс пользователя
-#         await db_manager.update_user_balance(chat_id, amount)
-#         return True
-#     elif payment["status"] == "canceled":
-#         logger.info(f"Платеж {payment_id} был отменен для пользователя {chat_id}.")
-#         return False
