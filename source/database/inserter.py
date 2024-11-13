1e0f2e51a1b127415e0f5e67d05e579459ae5b35
Ardgellan <aegiev01@gmail.com>
2024-11-13 22:40:12 +0300

    separation_50

--- source/database/inserter.py
+++ source/database/inserter.py
@@ -46,11 +46,11 @@ class Inserter(DatabaseConnector):
     #     logger.debug(f"VPN config {config_uuid} was inserted")
 
     async def insert_new_vpn_config(
-        self, user_id: int, config_name: str, config_uuid: str, server_domain: str, conn=None
+        self, user_id: int, config_name: str, config_uuid: str, server_domain: str, country_name: str, country_code: str, conn=None
     ):
         query = f"""--sql
-            INSERT INTO vpn_configs (user_id, config_name, config_uuid, server_domain)
-            VALUES ({user_id}, '{config_name}', '{config_uuid}', '{server_domain}');
+            INSERT INTO vpn_configs (user_id, config_name, config_uuid, server_domain, country_name, country_code)
+            VALUES ({user_id}, '{config_name}', '{config_uuid}', '{server_domain}', '{country_name}', '{country_code}');
         """
 
         if conn:
