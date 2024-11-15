d44c9f6ab86237553d800e818ae40c29000cc2e9
Ardgellan <aegiev01@gmail.com>
2024-11-15 20:21:24 +0300

    separation_112

--- source/utils/shedulers/subscription_checker.py
+++ source/utils/shedulers/subscription_checker.py
@@ -18,7 +18,7 @@ class SubscriptionChecker:
         self._messages_limits_counter = 0
         self._scheduler = AsyncIOScheduler()
         # start checking subscriptions every day at 12:00
-        self._scheduler.add_job(self._check_subscriptions, "cron", hour=18, minute=42)
+        self._scheduler.add_job(self._check_subscriptions, "cron", hour=20, minute=45)
         self._scheduler.start()
         logger.info("Subscription checker was started...")
 
