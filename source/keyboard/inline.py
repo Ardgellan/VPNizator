51f849b6ad628cfbe3fe26529d0b18626d3023e1
Ardgellan <aegiev01@gmail.com>
2025-02-02 17:06:32 +0300

    finalization_45

--- source/keyboard/inline.py
+++ source/keyboard/inline.py
@@ -361,7 +361,7 @@ async def download_app_for_connect_to_vpn_keyboard(
 ) -> InlineKeyboardMarkup:
     match platform:
         case "ios":
-            url = "https://apps.apple.com/us/app/streisand/id6450534064"
+            url = "https://apps.apple.com/ru/app/v2raytun/id6476628951"
 
         case "android":
             url = "https://play.google.com/store/apps/details?id=com.v2ray.ang"
