d85c568d35b2c444a9a9838c684c0b199dfa5a3f
Ardgellan <aegiev01@gmail.com>
2025-03-15 13:21:48 +0300

    hotfix_1

--- source/keyboard/inline.py
+++ source/keyboard/inline.py
@@ -368,7 +368,7 @@ async def download_app_for_connect_to_vpn_keyboard(
 ) -> InlineKeyboardMarkup:
     match platform:
         case "ios":
-            url = "https://apps.apple.com/us/app/hiddify-proxy-vpn/id6596777532"
+            url = "https://apps.apple.com/ru/app/v2raytun/id6476628951"
 
         case "android":
             url = "https://play.google.com/store/apps/details?id=app.hiddify.com&hl=en"
