e9af868280db5e02cfeaf25eee2ea6091547f23a
ArdGellan <aegiev01@gmail.com>
2025-07-03 18:59:58 +0300

    new_mailer_12

--- source/database/selector.py
+++ source/database/selector.py
@@ -632,5 +632,5 @@ class Selector(DatabaseConnector):
             AND u.is_banned = FALSE;
         """
         result = await self._execute_query(query)
-        logger.debug(f"Users for domain '{domain}': {result}")
+        # logger.debug(f"Users for domain '{domain}': {result}")
         return [record[0] for record in result] if result else []
