8c17901a94c3828780a7e41cb503d5d81f5063e3
ArdGellan <aegiev01@gmail.com>
2025-07-21 18:39:14 +0300

    first_stage_refactoring_8

--- source/keyboard/inline.py
+++ source/keyboard/inline.py
@@ -100,35 +100,6 @@ async def faq_keyboard(language_code: str):
     return keyboard
 
 
-async def faq_keyboard(language_code: str):
-    keyboard = InlineKeyboardMarkup(row_width=1)
-    buttons = [
-        InlineKeyboardButton(
-            text=localizer.get_user_localized_text(
-                user_language_code=language_code,
-                text_localization=localizer.button.write_to_support,
-            ),
-            callback_data="support_faq",
-        ),
-        InlineKeyboardButton(
-            text=localizer.get_user_localized_text(
-                user_language_code=language_code,
-                text_localization=localizer.button.write_to_support,
-            ),
-            callback_data="create_support_ticket",
-        ),
-        # Добавь больше вопросов по необходимости
-    ]
-
-    for button in buttons:
-        keyboard.insert(button)
-
-    keyboard = await insert_button_back_to_main_menu(keyboard=keyboard, language_code=language_code)
-    
-    return keyboard
-
-
-
 async def start_menu_kb(language_code: str, user_id: int):
     keyboard = InlineKeyboardMarkup(row_width=1)
     buttons = [
